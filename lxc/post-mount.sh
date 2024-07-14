78f70bf1964c473678553b4784be1ba902248fe7
SupeChicken666 <supechicken666@gmail.com>
2024-07-14 18:22:17 +0800

    Update post-mount.sh

--- lxc/post-mount.sh
+++ lxc/post-mount.sh
@@ -1,11 +1,3 @@
 #!/system/bin/sh -eu
 
-if [ -b /dev/block/nvme0n1p1 ] && [ -b /dev/block/mmcblk0p1 ]; then
-  mount /dev/block/nvme0n1p1 /data/local
-  mount /dev/block/mmcblk0p1 /data/sdcard
-fi
-
-if [ ! -f /data/no-lxc ]; then
-  lxc-start openwrt || true
-  lxc-start ubuntu || true
-fi
+[ ! -f /data/no-lxc ] && lxc-autostart --groups autoboot
